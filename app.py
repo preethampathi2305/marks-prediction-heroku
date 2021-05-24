d79643f34abefbf4732bb8b4eb97bffcdbf088aa
Preetham Pathi <62016121+preethampathi2305@users.noreply.github.com>
2021-05-25 01:23:26 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+#loading model
+import pickle
+with open("model.pkl","rb") as f:
+    lr=pickle.load(f)
+
+from flask import Flask,render_template,redirect
+from flask.globals import request
+#__name__=__main__
+app = Flask(__name__)
+
+#sending arguments to html
+#fav_artists=['dr.dre','kendrick lamar','ice cube','eminem']
+
+'''@app.route('/') 
+def hello():
+    return render_template("home.html")'''
+
+'''@app.route('/about')
+def about():
+    return "<h1> About us</h1>"'''
+
+'''@app.route('/home')
+def home():
+    return redirect('/')'''
+#getting input and returning output
+@app.route('/',methods=['POST'])
+def submitting():
+    if request.method=='POST':
+        num=request.form['exname']
+    return render_template("home.html",lr.predict([[num]]))
+
+
+
+if __name__ == '__main__':
+    app.debug=True # with this restarting server for every change is not needed it updates automatically
+    app.run()
